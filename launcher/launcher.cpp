4f417fa537652b24c81b6f234ef68a285a2f251e
Olivier Tilloy <olivier.tilloy@canonical.com>
2010-08-30 12:46:36 +0200

    Build a launcher executable.

---
+++ launcher/launcher.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2010 Canonical, Ltd.
+ *
+ * Authors:
+ *  Olivier Tilloy <olivier.tilloy@canonical.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QApplication>
+#include <QDeclarativeView>
+
+int main(int argc, char *argv[])
+{
+    QApplication application(argc, argv);
+
+    QDeclarativeView view;
+    view.setSource(QUrl("./Launcher.qml"));
+
+    return application.exec();
+}
+
