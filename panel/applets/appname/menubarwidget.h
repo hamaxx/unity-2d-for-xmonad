9590a0e8d49800082b407be9cf90ce5466cdbeb1
Michael Casadevall <mcasadevall@ubuntu.com>
2011-01-14 17:41:03 -0800

    Put proper licenses on ALL headers

--- panel/applets/appname/menubarwidget.h
+++ panel/applets/appname/menubarwidget.h
@@ -6,8 +6,19 @@
  * Authors:
  * - Aurélien Gâteau <aurelien.gateau@canonical.com>
  *
- * License: GPL v3
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #ifndef MENUBARWIDGET_H
 #define MENUBARWIDGET_H
 
