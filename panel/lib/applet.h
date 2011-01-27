e0f0fcc8a6fc2c1b82ef1c30db38aa086f186f33
Michael Terry <mike@mterry.name>
2011-01-27 15:13:23 -0500

    convert debian/copyright to DEP5, relicense LGPL bits as GPL-3

--- panel/lib/applet.h
+++ panel/lib/applet.h
@@ -7,7 +7,7 @@
  * - Aurélien Gâteau <aurelien.gateau@canonical.com>
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published 
+ * it under the terms of the GNU General Public License as published 
  * by the Free Software Foundation; version 3.
  *
  * This program is distributed in the hope that it will be useful,
@@ -15,7 +15,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
