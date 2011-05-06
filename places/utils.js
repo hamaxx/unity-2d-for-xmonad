fbf76ae2b822e53f3f3fc83628974c726b7ff79b
Olivier Tilloy <olivier.tilloy@canonical.com>
2011-05-06 10:39:42 +0200

    House keeping: removed an unused file.

--- places/utils.js
+++
@@ -1,6 +0,0 @@
-function launchApplicationFromDesktopFile(desktopFile, parentItem) {
-    var application = Qt.createQmlObject("import UnityApplications 1.0; LauncherApplication {desktop_file: \"%1\"}".arg(desktopFile),
-                                         parentItem, "");
-    application.activate()
-    application.destroy()
-}
