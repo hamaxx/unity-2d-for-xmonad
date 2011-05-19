f7ccc4716f304db7dbae6a4401917994423a822e
Florian Boucault <florian@boucault.net>
2011-05-19 14:39:42 +0200

    LauncherUtility: - added copyright headers - changed class to namespace - cleaned includes

--- launcher/UnityApplications/launcherutility.cpp
+++ launcher/UnityApplications/launcherutility.cpp
@@ -1,12 +1,31 @@
-
-extern "C" {
-#include <libsn/sn.h>
-}
+/*
+ * Copyright (C) 2010 Canonical, Ltd.
+ *
+ * Authors:
+ *  Florian Boucault <florian.boucault@canonical.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include "launcherutility.h"
 
-void
-LauncherUtility::moveViewportToWindow(WnckWindow* window)
+#include <X11/X.h>
+
+extern "C" {
+#include <libwnck/libwnck.h>
+}
+
+void LauncherUtility::moveViewportToWindow(WnckWindow* window)
 {
     WnckWorkspace* workspace = wnck_window_get_workspace(window);
     WnckScreen* screen = wnck_window_get_screen(window);
@@ -31,8 +50,7 @@ LauncherUtility::moveViewportToWindow(WnckWindow* window)
                                       viewport_row * screen_height);
 }
 
-void
-LauncherUtility::showWindow(WnckWindow* window)
+void LauncherUtility::showWindow(WnckWindow* window)
 {
     WnckWorkspace* workspace = wnck_window_get_workspace(window);
 
--- launcher/UnityApplications/launcherutility.h
+++ launcher/UnityApplications/launcherutility.h
@@ -1,15 +1,30 @@
+/*
+ * Copyright (C) 2010 Canonical, Ltd.
+ *
+ * Authors:
+ *  Florian Boucault <florian.boucault@canonical.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef LAUNCHERUTILITY_H
 #define LAUNCHERUTILITY_H
 
-#include <gio/gdesktopappinfo.h>
-#include <libwnck/libwnck.h>
+typedef struct _WnckWindow WnckWindow;
 
-class LauncherUtility {
-
-public:
-    static void showWindow(WnckWindow* window);
-    static void moveViewportToWindow(WnckWindow* window);
-};
+namespace LauncherUtility {
+    void showWindow(WnckWindow* window);
+    void moveViewportToWindow(WnckWindow* window);
+}
 
 #endif // LAUNCHERUTILITY_H
