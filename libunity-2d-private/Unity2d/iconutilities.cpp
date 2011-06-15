5791680471841a9a61ef89de9e1be55c4fbbd4ea
Florian Boucault <florian@boucault.net>
2011-06-15 15:17:43 +0200

    Properly credited libunity-2d-private/Unity2d/iconutilities.cpp

--- libunity-2d-private/Unity2d/iconutilities.cpp
+++ libunity-2d-private/Unity2d/iconutilities.cpp
@@ -1,6 +1,9 @@
 /*
  * Copyright (C) 2011 Canonical, Ltd.
  *
+ * Authors:
+ *  Ugo Riboni <ugo.riboni@canonical.com>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 3.
