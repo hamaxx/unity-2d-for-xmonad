00ce514c57dad730de7a021d9a8b5f2e97170d8f
Florian Boucault <florian@boucault.net>
2011-07-05 18:40:08 +0200

    [dash] Fix use of opengl rendering backend: artifacts were displayed, even more so with transparency enabled.
    The cause of the issue was that the QGLWidget viewport was set on the QDeclarativeView
    before having setup the QGLWidget's flags.
    Implementation of Unity2dDeclarativeView is much clarified as a result and this change
    impacts all the components using it:
    - launcher
    - dash
    - spread
    
    Synchronization to VBlank has also been activated.

--- libunity-2d-private/src/unity2ddeclarativeview.cpp
+++ libunity-2d-private/src/unity2ddeclarativeview.cpp
@@ -2,10 +2,9 @@
 #include <QGLWidget>
 
 Unity2DDeclarativeView::Unity2DDeclarativeView(QWidget *parent) :
-    QDeclarativeView(parent), m_useOpenGL(false)
+    QDeclarativeView(parent), m_useOpenGL(false), m_transparentBackground(false)
 {
-    setAttribute(Qt::WA_NoSystemBackground);
-    setTransparentBackground(false);
+    setupViewport();
 }
 
 bool Unity2DDeclarativeView::useOpenGL() const
@@ -19,20 +18,9 @@ void Unity2DDeclarativeView::setUseOpenGL(bool useOpenGL)
         return;
     }
 
-    if (useOpenGL) {
-        QGLFormat format = QGLFormat::defaultFormat();
-        format.setSampleBuffers(false);
-
-        QGLWidget *glWidget = new QGLWidget(format);
-        /* TODO: possibly faster, to be tested */
-        //glWidget->setAutoFillBackground(false);
-
-        setViewport(glWidget);
-    } else {
-        setViewport(0);
-    }
-
     m_useOpenGL = useOpenGL;
+    setupViewport();
+
     Q_EMIT useOpenGLChanged(useOpenGL);
 }
 
@@ -43,12 +31,58 @@ bool Unity2DDeclarativeView::transparentBackground() const
 
 void Unity2DDeclarativeView::setTransparentBackground(bool transparentBackground)
 {
-    setAttribute(Qt::WA_TranslucentBackground, transparentBackground);
-    viewport()->setAttribute(Qt::WA_TranslucentBackground, transparentBackground);
-    setAttribute(Qt::WA_OpaquePaintEvent, !transparentBackground);
+    if (transparentBackground == m_transparentBackground) {
+        return;
+    }
 
     m_transparentBackground = transparentBackground;
+    setupViewport();
+
     Q_EMIT transparentBackgroundChanged(transparentBackground);
+}
+
+void Unity2DDeclarativeView::setupViewport()
+{
+    if (m_useOpenGL) {
+        QGLFormat format = QGLFormat::defaultFormat();
+        format.setSampleBuffers(false);
+        /* Synchronise rendering with vblank */
+        format.setSwapInterval(1);
+
+        QGLWidget *glWidget = new QGLWidget(format);
+        /* TODO: possibly faster, to be tested, only works with non transparent windows */
+        //glWidget->setAutoFillBackground(false);
+
+        if (m_transparentBackground) {
+            glWidget->setAttribute(Qt::WA_TranslucentBackground, true);
+            setAttribute(Qt::WA_TranslucentBackground, true);
+            /* automatically set by setting WA_TranslucentBackground */
+            setAttribute(Qt::WA_NoSystemBackground, true);
+            setAttribute(Qt::WA_OpaquePaintEvent, true);
+        } else {
+            glWidget->setAttribute(Qt::WA_TranslucentBackground, false);
+            setAttribute(Qt::WA_TranslucentBackground, false);
+            setAttribute(Qt::WA_NoSystemBackground, true);
+            setAttribute(Qt::WA_OpaquePaintEvent, true);
+        }
+
+        setViewport(glWidget);
+    } else {
+        setViewport(0);
+
+        if (m_transparentBackground) {
+            viewport()->setAttribute(Qt::WA_TranslucentBackground, true);
+            setAttribute(Qt::WA_TranslucentBackground, true);
+            /* automatically set by setting WA_TranslucentBackground */
+            setAttribute(Qt::WA_NoSystemBackground, true);
+            setAttribute(Qt::WA_OpaquePaintEvent, false);
+        } else {
+            viewport()->setAttribute(Qt::WA_TranslucentBackground, false);
+            setAttribute(Qt::WA_TranslucentBackground, false);
+            setAttribute(Qt::WA_NoSystemBackground, true);
+            setAttribute(Qt::WA_OpaquePaintEvent, true);
+        }
+    }
 }
 
 #include <unity2ddeclarativeview.moc>
