92ef88877e4c90194725a30ed98025557f75201c
Aurélien Gâteau <aurelien.gateau@canonical.com>
2011-07-18 08:47:29 -0500

    [libunity-2d] Introduce Unity2dApplication::earlySetup()
    This method factorizes all the common initialization which was previously
    duplicated in the main() of all unity-2d-* binaries.

--- libunity-2d-private/src/unity2dapplication.cpp
+++ libunity-2d-private/src/unity2dapplication.cpp
@@ -22,13 +22,61 @@
 // Self
 #include "unity2dapplication.h"
 
+// libunity-2d
+#include <debug_p.h>
+#include <unity2ddebug.h>
+
 // Qt
+#include <QWindowsStyle>
+
+// GTK
+#include <gtk/gtk.h>
 
 AbstractX11EventFilter::~AbstractX11EventFilter()
 {
     Unity2dApplication* application = Unity2dApplication::instance();
     if (application != NULL) {
         application->removeX11EventFilter(this);
+    }
+}
+
+static bool arrayContains(char** begin, char** end, const char* string)
+{
+    for (char** ptr = begin; ptr != end; ++ptr) {
+        if (strcmp(*ptr, string) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void Unity2dApplication::earlySetup(int& argc, char** argv)
+{
+    // Parts of unity-2d uses GTK so it needs to be initialized
+    gtk_init(&argc, &argv);
+
+    Unity2dDebug::installHandlers();
+
+    /* When the environment variable QT_GRAPHICSSYSTEM is not set, force
+     * graphics system to 'raster' instead of the default 'native' which on X11
+     * is 'XRender'.  'XRender' defaults to using a TrueColor visual. We do
+     * _not_ mimick that behaviour with 'raster' by calling
+     * QApplication::setColorSpec because of bugs where some pixmaps become
+     * blueish or black rectangular artifacts were appearing randomly:
+     *
+     *  https://bugs.launchpad.net/unity-2d/+bug/689877
+     *  https://bugs.launchpad.net/unity-2d/+bug/734143
+     */
+    if(getenv("QT_GRAPHICSSYSTEM") == 0) {
+        QApplication::setGraphicsSystem("raster");
+    }
+
+    /* Unless style has been specified in args, set default Qt style to
+     * QWindowStyle to avoid loading QGtkStyle. We don't want to load QGtkStyle
+     * because it uses libgtk2, which causes conflicts with our gtk3 code.
+     */
+    if (!arrayContains(argv, argv + argc, "-style")) {
+        QApplication::setStyle(new QWindowsStyle);
     }
 }
 
