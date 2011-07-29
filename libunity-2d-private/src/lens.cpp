50559bf427c7a5908a71a11113256cfb30f6548d
Florian Boucault <florian@boucault.net>
2011-07-30 01:53:04 +0200

    Lens: adapted for unity-core API changes

--- libunity-2d-private/src/lens.cpp
+++ libunity-2d-private/src/lens.cpp
@@ -45,9 +45,9 @@ QString Lens::name() const
     return QString::fromStdString(m_unityLens->name());
 }
 
-QString Lens::icon() const
+QString Lens::iconHint() const
 {
-    return QString::fromStdString(m_unityLens->icon());
+    return QString::fromStdString(m_unityLens->icon_hint());
 }
 
 QString Lens::description() const
@@ -65,27 +65,64 @@ bool Lens::visible() const
     return m_unityLens->visible();
 }
 
+bool Lens::searchInGlobal() const
+{
+    return m_unityLens->search_in_global();
+}
+
 QString Lens::shortcut() const
 {
     return QString::fromStdString(m_unityLens->shortcut());
 }
 
+bool Lens::connected() const
+{
+    return m_unityLens->connected();
+}
+
+bool Lens::active() const
+{
+    return m_unityLens->active();
+}
+
+void Lens::globalSearch(QString search_string)
+{
+    m_unityLens->GlobalSearch(search_string.toStdString());
+}
+
+void Lens::search(QString search_string)
+{
+    m_unityLens->Search(search_string.toStdString());
+}
+
 void Lens::setUnityLens(unity::dash::Lens::Ptr lens)
 {
     if (m_unityLens != NULL) {
-        // FIXME: should disconnect from m_unityLens's properties changed signal
+        // FIXME: should disconnect from m_unityLens's signals
     }
 
     m_unityLens = lens;
+
+    /* Property change signals */
     m_unityLens->id.changed.connect(sigc::mem_fun(this, &Lens::idChanged));
     m_unityLens->dbus_name.changed.connect(sigc::mem_fun(this, &Lens::dbusNameChanged));
     m_unityLens->dbus_path.changed.connect(sigc::mem_fun(this, &Lens::dbusPathChanged));
     m_unityLens->name.changed.connect(sigc::mem_fun(this, &Lens::nameChanged));
-    m_unityLens->icon.changed.connect(sigc::mem_fun(this, &Lens::iconChanged));
+    m_unityLens->icon_hint.changed.connect(sigc::mem_fun(this, &Lens::iconHintChanged));
     m_unityLens->description.changed.connect(sigc::mem_fun(this, &Lens::descriptionChanged));
     m_unityLens->search_hint.changed.connect(sigc::mem_fun(this, &Lens::searchHintChanged));
     m_unityLens->visible.changed.connect(sigc::mem_fun(this, &Lens::visibleChanged));
+    m_unityLens->search_in_global.changed.connect(sigc::mem_fun(this, &Lens::searchInGlobalChanged));
     m_unityLens->shortcut.changed.connect(sigc::mem_fun(this, &Lens::shortcutChanged));
+    m_unityLens->connected.changed.connect(sigc::mem_fun(this, &Lens::connectedChanged));
+    //m_unityLens->results.changed.connect(sigc::mem_fun(this, &Lens::resultsChanged));
+    //m_unityLens->global_results.changed.connect(sigc::mem_fun(this, &Lens::globalResultsChanged));
+    //m_unityLens->categories.changed.connect(sigc::mem_fun(this, &Lens::categoriesChanged));
+    m_unityLens->active.changed.connect(sigc::mem_fun(this, &Lens::activeChanged));
+
+    /* Signals forwarding */
+    m_unityLens->search_finished.connect(sigc::mem_fun(this, &Lens::searchFinished));
+    m_unityLens->global_search_finished.connect(sigc::mem_fun(this, &Lens::globalSearchFinished));
 }
 
 #include "lens.moc"
--- libunity-2d-private/src/lens.h
+++ libunity-2d-private/src/lens.h
@@ -37,11 +37,20 @@ class Lens : public QObject
     Q_PROPERTY(QString dbusName READ dbusName NOTIFY dbusNameChanged)
     Q_PROPERTY(QString dbusPath READ dbusPath NOTIFY dbusPathChanged)
     Q_PROPERTY(QString name READ name NOTIFY nameChanged)
-    Q_PROPERTY(QString icon READ icon NOTIFY iconChanged)
+    Q_PROPERTY(QString iconHint READ iconHint NOTIFY iconHintChanged)
     Q_PROPERTY(QString description READ description NOTIFY descriptionChanged)
     Q_PROPERTY(QString searchHint READ searchHint NOTIFY searchHintChanged)
     Q_PROPERTY(bool visible READ visible NOTIFY visibleChanged)
+    Q_PROPERTY(bool searchInGlobal READ searchInGlobal NOTIFY searchInGlobalChanged)
     Q_PROPERTY(QString shortcut READ shortcut NOTIFY shortcutChanged)
+    Q_PROPERTY(bool connected READ connected NOTIFY connectedChanged)
+//    nux::RWProperty<Results::Ptr> results;
+//    nux::RWProperty<Results::Ptr> global_results;
+//    nux::RWProperty<Categories::Ptr> categories;
+//    Q_PROPERTY(Results* results READ results NOTIFY resultsChanged)
+//    Q_PROPERTY(Results* globalResults READ globalResults NOTIFY globalResultsChanged)
+//    Q_PROPERTY(Categories* categories READ categories NOTIFY categoriesChanged)
+    Q_PROPERTY(bool active READ active NOTIFY activeChanged)
 
 public:
     explicit Lens(QObject *parent = 0);
@@ -51,12 +60,17 @@ public:
     QString dbusName() const;
     QString dbusPath() const;
     QString name() const;
-    QString icon() const;
+    QString iconHint() const;
     QString description() const;
     QString searchHint() const;
     bool visible() const;
+    bool searchInGlobal() const;
     QString shortcut() const;
+    bool connected() const;
+    bool active() const;
 
+    void globalSearch(QString search_string);
+    void search(QString search_string);
     void setUnityLens(unity::dash::Lens::Ptr lens);
 
 Q_SIGNALS:
@@ -64,11 +78,16 @@ Q_SIGNALS:
     void dbusNameChanged(std::string);
     void dbusPathChanged(std::string);
     void nameChanged(std::string);
-    void iconChanged(std::string);
+    void iconHintChanged(std::string);
     void descriptionChanged(std::string);
     void searchHintChanged(std::string);
     void visibleChanged(bool);
+    void searchInGlobalChanged(bool);
     void shortcutChanged(std::string);
+    void connectedChanged(bool);
+    void activeChanged(bool);
+    void searchFinished(std::string const&);
+    void globalSearchFinished(std::string const&);
 
 private:
     unity::dash::Lens::Ptr m_unityLens;
