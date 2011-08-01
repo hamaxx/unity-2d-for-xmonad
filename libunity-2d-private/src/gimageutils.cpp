88774bfaee5c923712c2737d9dab02ad321cd0f1
Aurelien Gateau <aurelien.gateau@canonical.com>
2011-08-01 12:08:29 +0200

    [libunity-2d] GScopedPointer-ified, coding style fixes.

--- libunity-2d-private/src/gimageutils.cpp
+++ libunity-2d-private/src/gimageutils.cpp
@@ -23,6 +23,7 @@
 
 // Local
 #include <debug_p.h>
+#include <gscopedpointer.h>
 
 // Qt
 #include <QByteArray>
@@ -39,35 +40,33 @@ QImage imageForIconString(const QString& name, int size, GtkIconTheme* theme)
     if (!theme) {
         theme = gtk_icon_theme_get_default();
     }
+    QByteArray utf8Name = name.toUtf8();
 
     /* Load the icon by creating a GIcon from the string icon_name.
        icon_name can contain more than a simple icon name but possibly
        a string as returned by g_icon_to_string().
     */
-    QByteArray utf8Name = name.toUtf8();
-    GIcon *g_icon = g_icon_new_for_string(utf8Name.data(), NULL);
-    GtkIconInfo *icon_info = gtk_icon_theme_lookup_by_gicon(theme, g_icon,
-                                                            size,
-                                                            (GtkIconLookupFlags)0);
-    g_object_unref(g_icon);
+    GObjectScopedPointer<GIcon> icon(g_icon_new_for_string(utf8Name.data(), NULL));
+    GScopedPointer<GtkIconInfo, gtk_icon_info_free> iconInfo;
+    iconInfo.reset(gtk_icon_theme_lookup_by_gicon(
+        theme,
+        icon.data(),
+        size,
+        (GtkIconLookupFlags)0)
+        );
 
-    if (icon_info == NULL) {
+    if (!iconInfo) {
         UQ_WARNING << "Failed to find icon:" << name;
         return QImage();
     }
 
-    GdkPixbuf *pixbuf = gtk_icon_info_load_icon(icon_info, NULL);
-    gtk_icon_info_free(icon_info);
-
-    if (pixbuf == NULL) {
+    GObjectScopedPointer<GdkPixbuf> pixbuf(gtk_icon_info_load_icon(iconInfo.data(), NULL));
+    if (!pixbuf) {
         UQ_WARNING << "Failed to load icon:" << name;
         return QImage();
     }
 
-    QImage image = imageForPixbuf(pixbuf);
-    g_object_unref(pixbuf);
-
-    return image;
+    return imageForPixbuf(pixbuf.data());
 }
 
 QImage imageForPixbuf(const GdkPixbuf* pixbuf)
