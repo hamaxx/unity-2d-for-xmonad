ae3a2e6f8b33a1f2f97623e110303817f5ef95ce
Gerry Boland <gerboland@gmail.com>
2011-09-07 12:32:02 +0200

    [launcher] Unity uses the Trash icon supplied by the default theme. Unity2D now does the same.

--- libunity-2d-private/src/trash.cpp
+++ libunity-2d-private/src/trash.cpp
@@ -364,9 +364,9 @@ void
 Trash::updateTrashIcon(void)
 {
     if(count() == 0) {
-        m_iconName = "unity-icon-theme/user-trash"; }
+        m_iconName = "user-trash"; }
     else {
-        m_iconName = "unity-icon-theme/user-trash-full"; }
+        m_iconName = "user-trash-full"; }
 }
 
 #include "trash.moc"
