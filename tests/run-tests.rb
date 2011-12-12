cb5871fa62626cfa94e5fb86e32ade04d2ccd3a0
Gerry Boland <gerry.boland@canonical.com>
2011-12-12 22:32:54 +0100

    Add Copyright notice

--- tests/run-tests.rb
+++ tests/run-tests.rb
@@ -1,5 +1,27 @@
 #!/usr/bin/env ruby
-#
+=begin
+/*
+ * This file is part of unity-2d
+ *
+ * Copyright 2011 Canonical Ltd.
+ *
+ * Authors:
+ * - Gerry Boland <gerry.boland@canonical.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+=end
+
 # Testability test suite execution script
 # 
 # Run all tests with
