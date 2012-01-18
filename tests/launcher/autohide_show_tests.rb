88d4863bf6673c5902b2e07acca050bb2636b3eb
Albert Astals <albert.astals@canonical.com>
2012-01-18 14:59:58 +0100

    Test that the launcher is around for 1 second after removign an application tile

--- tests/launcher/autohide_show_tests.rb
+++ tests/launcher/autohide_show_tests.rb
@@ -31,6 +31,7 @@ require 'tmpwindow'
 ############################# Test Suite #############################
 context "Launcher Autohide and Show Tests" do
   WIDTH = 65 #launcher bar width
+  launcher_favorites = ""
 
   # Run once at the beginning of this test suite
   startup do
@@ -50,6 +51,8 @@ context "Launcher Autohide and Show Tests" do
     #Ensure mouse out of the way
     XDo::Mouse.move(200,200,10,true)    
 
+    launcher_favorites = `dconf read /desktop/unity/launcher/favorites`
+
     # Execute the application 
     @sut = TDriver.sut(:Id => "sut_qt")    
     @app = @sut.run( :name => UNITY_2D_LAUNCHER, 
@@ -65,6 +68,7 @@ context "Launcher Autohide and Show Tests" do
     #@app.close        
     #Need to kill Launcher as it does not shutdown when politely asked
     system "pkill -nf unity-2d-launcher"
+    system "dconf write /desktop/unity/launcher/favorites \"" + launcher_favorites + "\""
   end
 
   #####################################################################################
@@ -373,6 +377,54 @@ context "Launcher Autohide and Show Tests" do
       @app.Unity2dPanel()['x_absolute'].to_i
     }
     xid.close!
+  end
+
+  # Test case objectives:
+  #   * Launcher is around for 1 second after removing a tile
+  # Pre-conditions
+  #   * Desktop with at least one application not running
+  # Test steps
+  #   * Open application in position overlapping Launcher
+  #   * Verify Launcher hiding
+  #   * Right click on a non running application tile
+  #   * Click on the last menu item (Remove)
+  #   * Verify Launcher stays away for a second
+  # Post-conditions
+  #   * None
+  # References
+  #   * None
+  test "Launcher hide delay on tile removal" do
+    xid = TmpWindow.open_window_at(10,100)
+    tile = ""
+    found_tile = false
+    launcher_favorites[1..-3].split(',').each { |i|
+      if !found_tile
+        tile = @app.LauncherList( :name => 'main' ).LauncherList( :desktopFile => '/usr/share/applications/' + i.strip[1..-2] )
+        if tile['running'] == "false"
+          found_tile = true
+        end
+      end
+    }
+    verify( 0, 'Could not find any non running application tile to remove' ) { found_tile }
+    if found_tile
+      XDo::Mouse.move(0, 200, 0, true)
+      verify_equal( 0, TIMEOUT, 'Launcher hiding when mouse at left edge of screen' ) {
+        @app.Unity2dPanel()['x_absolute'].to_i
+      }
+      XDo::Mouse.move(tile['x_absolute'].to_i + 1, tile['y_absolute'].to_i + 1, 0, true)
+      XDo::Mouse.click(nil, nil, :right)
+      menu = @app.LauncherContextualMenu( :folded => false );
+      XDo::Mouse.move(menu['x_absolute'].to_i + 20, menu['y_absolute'].to_i + menu['height'].to_i - 10, 0, true)
+      XDo::Mouse.click()
+      sleep 0.9
+      verify_equal( 0, 0, 'Launcher hiding after icon removal, should be visible for 1 second' ) {
+        @app.Unity2dPanel()['x_absolute'].to_i
+      }
+      verify_equal( -WIDTH, TIMEOUT, 'Launcher visible with window in the way, should be hidden' ) {
+        @app.Unity2dPanel()['x_absolute'].to_i
+      }
+      xid.close!
+    end
   end
 
 end
