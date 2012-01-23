5e4d7bf1f2f1057113304d01f930e087056d73fc
Gerry Boland <gerry.boland@canonical.com>
2012-01-23 17:34:47 +0100

    Add SUT specification to XDo library - not elegant fix

--- tests/run-tests.rb
+++ tests/run-tests.rb
@@ -63,6 +63,13 @@ include TDriverVerify
 # Require unit test framework: This enables execution of test cases and also includes assertions (Test::Unit::Assertions)
 require 'testhelper'
 
+#Establish connection to qttasserver
+$SUT = TDriver.sut(:Id => "sut_qt")
+
+#Include the XDo common library
+require $library_path + '/xdo/_xdo'
+XDo.sut = $SUT
+
 # List of directories in which to search for test cases
 test_directories = ['launcher', 'panel', 'places', 'spread', 'window-manager', 'other']
 
