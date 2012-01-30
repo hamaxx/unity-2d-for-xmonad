9ac85880ee3d9572683571964916f6546496228d
Albert Astals <albert.astals@canonical.com>
2012-01-30 09:51:44 +0100

    remove "warning: already initialized constant BINARY_DIR"

--- tests/run-tests.rb
+++ tests/run-tests.rb
@@ -41,7 +41,6 @@ $LOAD_PATH.unshift $library_path
 # If cmake was called, obtain the path to the built binary directory. If not, we test the
 # installed applications instead
 binary_dir_file = $library_path + '/../binary_dir.txt'
-BINARY_DIR = ""
 if File.exists?(binary_dir_file)
     BINARY_DIR = File.open(binary_dir_file).first.strip
     puts 'Running tests on applications contained within ' + BINARY_DIR
@@ -49,6 +48,7 @@ if File.exists?(binary_dir_file)
     UNITY_2D_PANEL = BINARY_DIR + '/panel/app/unity-2d-panel'
     UNITY_2D_SPREAD = BINARY_DIR + '/spread/app/unity-2d-spread'
 else
+    BINARY_DIR = ""
     puts 'NOTICE: source not configured, tests will be carried out on *installed* applications!'
     UNITY_2D_SHELL = 'unity-2d-shell'
     UNITY_2D_PANEL = 'unity-2d-panel'
