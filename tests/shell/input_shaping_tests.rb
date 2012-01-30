acd536018cfbee2b73a792f235d7d404c921717f
Albert Astals <albert.astals@canonical.com>
2012-01-30 18:26:15 +0100

    Add header

--- tests/shell/input_shaping_tests.rb
+++ tests/shell/input_shaping_tests.rb
@@ -1,3 +1,25 @@
+=begin
+/*
+ * This file is part of unity-2d
+ *
+ * Copyright 2011 Canonical Ltd.
+ *
+ * Authors:
+ * - Gerry Boland <gerry.boland@canonical.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+=end
 
 # We just need a temp file name but ruby insist on creating it, so we just delete it first.
 def tempfilename(base, extension = ".tmp")
