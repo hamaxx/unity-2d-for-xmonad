a11f53f84941df6c530925282a33befb8ed76200
Albert Astals <albert.astals@canonical.com>
2012-02-01 12:08:39 +0100

    Remove the Authors section as requested by Gerry

--- tests/launcher/autohide_show_tests_common.rb
+++ tests/launcher/autohide_show_tests_common.rb
@@ -4,9 +4,6 @@
  *
  * Copyright 2011 Canonical Ltd.
  *
- * Authors:
- * - Gerry Boland <gerry.boland@canonical.com>
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 3.
