a18e0c8f0bbeeb16aecb4620736928c7e0897974
Michał Sawicz <michal.sawicz@canonical.com>
2012-02-01 17:14:19 +0100

    [lib] remove dead code from DesktopInfo

--- libunity-2d-private/src/desktopinfo.cpp
+++ libunity-2d-private/src/desktopinfo.cpp
@@ -16,14 +16,8 @@ extern "C" {
 #include <QDesktopWidget>
 
 DesktopInfo::DesktopInfo(QObject *parent) :
-    QObject(parent),
-    m_activeWindow(0)
+    QObject(parent)
 {
-    WnckScreen *screen = wnck_screen_get_default();
-    g_signal_connect(G_OBJECT(screen), "active-window-changed",
-                     G_CALLBACK(DesktopInfo::onActiveWindowChanged), NULL);
-
-    updateActiveWindow(screen);
 }
 
 
@@ -31,30 +25,6 @@ DesktopInfo* DesktopInfo::instance()
 {
     static DesktopInfo* singleton = new DesktopInfo();
     return singleton;
-}
-
-void DesktopInfo::onActiveWindowChanged(WnckScreen *screen,
-                                       WnckWindow *previously_active_window,
-                                       gpointer    user_data)
-{
-    Q_UNUSED(previously_active_window);
-    Q_UNUSED(user_data);
-
-    DesktopInfo::instance()->updateActiveWindow(screen);
-}
-
-void DesktopInfo::updateActiveWindow(WnckScreen *screen)
-{
-    unsigned int activeWindow = 0;
-    WnckWindow *wnckActiveWindow = wnck_screen_get_active_window(screen);
-    if (wnckActiveWindow != NULL) {
-        activeWindow = wnck_window_get_xid(wnckActiveWindow);
-    }
-
-    if (activeWindow != m_activeWindow) {
-        m_activeWindow = activeWindow;
-        Q_EMIT activeWindowChanged(m_activeWindow);
-    }
 }
 
 bool DesktopInfo::isCompositingManagerRunning() const
--- libunity-2d-private/src/desktopinfo.h
+++ libunity-2d-private/src/desktopinfo.h
@@ -18,7 +18,6 @@ class DesktopInfo : public QObject
     Q_ENUMS(WorkspacesCorner)
 
     Q_PROPERTY(WorkspacesInfo *workspaces READ workspaces NOTIFY workspacesChanged)
-    Q_PROPERTY(unsigned int activeWindow READ activeWindow NOTIFY activeWindowChanged)
     Q_PROPERTY(bool isCompositingManagerRunning READ isCompositingManagerRunning
                                                 NOTIFY isCompositingManagerRunningChanged)
 
@@ -27,25 +26,17 @@ public:
 
     /* Getters */
     WorkspacesInfo *workspaces() { return &m_workspacesInfo; }
-    unsigned int activeWindow() const { return m_activeWindow; }
     bool isCompositingManagerRunning() const;
 
 Q_SIGNALS:
-    void activeWindowChanged(unsigned int activeWindow);
     void workspacesChanged(WorkspacesInfo *workspaces);
     void isCompositingManagerRunningChanged(bool);
 
 private:
     explicit DesktopInfo(QObject *parent = 0);
-    void updateActiveWindow(WnckScreen *screen);
-
-    static void onActiveWindowChanged(WnckScreen *screen,
-                                      WnckWindow *previously_active_window,
-                                      gpointer    user_data);
 
 private:
     WorkspacesInfo m_workspacesInfo;
-    unsigned int m_activeWindow;
 };
 
 #endif // DESKTOPINFO_H
