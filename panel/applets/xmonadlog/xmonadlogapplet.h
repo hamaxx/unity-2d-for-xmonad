08f4bca7e6d5ac0cd52e8ae1b7bc7d7b0c8c49c6
hamax <jure.ham@zemanta.com>
2012-02-02 23:23:30 +0100

    xmonad log pannel applet

---
+++ panel/applets/xmonadlog/xmonadlogapplet.h
@@ -0,0 +1,44 @@
+/*
+ * This file is part of unity-2d
+ *
+ * Copyright 2011 Canonical Ltd.
+ *
+ * Authors:
+ * - Ugo Riboni <ugo.riboni@canonical.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef XMONADLOGAPPLET_H
+#define XMONADLOGAPPLET_H
+
+// Qt
+#include <QLabel>
+
+// Unity-2d
+#include <panelapplet.h>
+
+using namespace Unity2d;
+
+class XmonadLogApplet : public Unity2d::PanelApplet
+{
+Q_OBJECT
+public:
+    XmonadLogApplet(Unity2dPanel* panel);
+
+private:
+    Q_DISABLE_COPY(XmonadLogApplet)
+    QLabel* x_log;
+};
+
+#endif /* XMONADLOGAPPLET_H */
