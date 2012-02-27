4f41759322ed2bd3501dab2783b68abbab038495
Gerry Boland <gerry.boland@canonical.com>
2012-02-27 12:51:09 +0100

    [lib] Add CursorShapeArea class to allow QML specify mouse pointer

---
+++ libunity-2d-private/src/cursorshapearea.h
@@ -0,0 +1,47 @@
+/*
+ * This file is part of unity-2d
+ *
+ * Copyright 2012 Canonical Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CURSORSHAPEAREA_H
+#define CURSORSHAPEAREA_H
+
+#include <QDeclarativeItem>
+
+/* Taken from a Qt Community Note by "njeisecke" at
+   https://developer.qt.nokia.com/doc/qt-4.7/qml-mousearea.html
+   made available under the Creative Commons Attribution-Share Alike 2.5 Generic licence*/
+
+class CursorShapeArea : public QDeclarativeItem
+{
+    Q_OBJECT
+
+    Q_PROPERTY(Qt::CursorShape cursorShape READ cursorShape WRITE setCursorShape NOTIFY cursorShapeChanged)
+
+public:
+    explicit CursorShapeArea(QDeclarativeItem *parent = 0);
+
+    Qt::CursorShape cursorShape() const;
+    Q_INVOKABLE void setCursorShape(Qt::CursorShape cursorShape);
+
+private:
+    Qt::CursorShape m_currentShape;
+
+Q_SIGNALS:
+    void cursorShapeChanged();
+};
+
+#endif // CURSORSHAPEAREA_H
