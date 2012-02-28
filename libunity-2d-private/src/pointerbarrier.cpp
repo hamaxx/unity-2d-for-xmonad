e4bedcc928a3b0b906d0ee8beecf011bf56be25f
Albert Astals <albert.astals@canonical.com>
2012-02-28 16:35:09 +0100

    Not all from Gerry

--- libunity-2d-private/src/pointerbarrier.cpp
+++ libunity-2d-private/src/pointerbarrier.cpp
@@ -1,9 +1,6 @@
 /*
  * Copyright (C) 2012 Canonical, Ltd.
  *
- * Authors:
- *  Gerry Boland <gerry.boland@canonical.com>
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; version 3.
