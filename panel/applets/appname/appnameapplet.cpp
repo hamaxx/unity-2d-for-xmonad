40442355c5d2e9622f820fce3e72c03f1a9078d5
Haggai Eran <haggai.eran@gmail.com>
2012-03-03 19:35:19 +0200

    [panel] Change panel window buttons alignment when in RTL locale (LP: #858053)

--- panel/applets/appname/appnameapplet.cpp
+++ panel/applets/appname/appnameapplet.cpp
@@ -133,8 +133,10 @@ protected:
                 pix = m_normalPix;
             }
         }
+        bool rtl = layoutDirection() == Qt::RightToLeft;
         int posX;
-        if (m_buttonType == PanelStyle::CloseWindowButton) {
+        if (!rtl && m_buttonType == PanelStyle::CloseWindowButton ||
+             rtl && m_buttonType != PanelStyle::CloseWindowButton) {
             posX = width() - pix.width();
         } else {
             posX = 0;
